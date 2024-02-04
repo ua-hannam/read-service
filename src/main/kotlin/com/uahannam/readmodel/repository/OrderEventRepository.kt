9e7f13343e957bc0acce217966bebc207fbbb237
서민재 <seominjae@seominjaeui-Macmini.local>
2024-02-04 22:02:55 +0900

    🚦 test: Kafka 메시지 중복 발행 동작 테스트 위해 코드 추가

--- src/main/kotlin/com/uahannam/readmodel/repository/OrderEventRepository.kt
+++ src/main/kotlin/com/uahannam/readmodel/repository/OrderEventRepository.kt
@@ -3,7 +3,6 @@ package com.uahannam.readmodel.repository
 import com.uahannam.readmodel.entity.OrderEventJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-
 interface OrderEventRepository : JpaRepository<OrderEventJpaEntity, Long> {
     fun findByEventUUID(uuid: String): OrderEventJpaEntity?
 }
